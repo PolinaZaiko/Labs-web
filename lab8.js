739de260fdffdbe2879e4e986ef944d80c472066
PolinaZaiko <124757757+PolinaZaiko@users.noreply.github.com>
2023-05-31 11:21:56 +0700

    Вывод даты

---
+++ lab8.js
@@ -0,0 +1,17 @@
+function showDate() {
+    let out = document.getElementById("current-date");
+    let today =new Date();
+    out.innerHTML = "Дата и время для русской локали: " + today.toLocaleDateString("ru-RU");
+    let out1 = document.getElementById("current-date1");
+    out1.innerHTML = "Дата и время для африканской локали: " + today.toLocaleDateString("af");
+    let out2 = document.getElementById("current-date2");
+    out2.innerHTML = "Дата и время для египетской локали: " + today.toLocaleDateString("ar-EG");
+    let out3 = document.getElementById("current-date3");
+    out3.innerHTML = "Дата и время для чешской локали: " + today.toLocaleDateString("cs");
+    let out4 = document.getElementById("current-date4");
+    out4.innerHTML = "Дата и время для германской локали: " + today.toLocaleDateString("de-DE");
+    let out5 = document.getElementById("current-date5");
+    out5.innerHTML = "Дата и время для канадской локали: " + today.toLocaleDateString("en-CA");
+    let out6 = document.getElementById("current-date6");
+    out6.innerHTML = "Дата и время для аргентинской локали: " + today.toLocaleDateString("es-AR");
+}
